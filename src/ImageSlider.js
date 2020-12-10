14f83d9ffbbbcd60fb4dc76828f248b02528a5f9
JlordA <james.ardery@gmail.com>
2020-12-09 20:04:55 -0500

    had to use the colution code and instructions were confusing

--- src/ImageSlider.js
+++ src/ImageSlider.js
@@ -1,1 +1,21 @@
+import React from 'react'
+const { render } = require("enzyme");
 // your ImageSlider code here!
+class ImageSlider extends React.Component {
+
+    constructor(props){
+        super()
+        this.state = { currentSlideIndex: 0 }
+    }
+    
+    
+    render(){
+        return (
+            <h1>I am on slide {this.state.currentSlideIndex}</h1>
+        )
+        
+    }
+}
+
+
+export default ImageSlider
